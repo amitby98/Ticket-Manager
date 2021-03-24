6b78b2392fde15d60a9bdd62187e807ebf88f975
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-03-25 01:27:43 +0200

    added PATCH method

--- app.js
+++ app.js
@@ -41,4 +41,40 @@ app.get("/api/tickets", async (req, res) => {
   }
 });
 
+// app.patch("/api/tickets/:ticketId/done", async (req, res) => {
+//   let ticketId = req.params.ticketId;
+//   console.log(result);
+//   Ticket.findByIdAndUpdate(ticketId, { $set: { done: true } })
+//     .then((result) => {
+//       if (!result) return res.status(404).json({ err: "cannot find ticket" });
+//       return res.json({ update: true });
+//     })
+//     .catch((err) => {
+//       if (err.name === "CastError")
+//         return res.status(400).json({ err: "Invalid Id" });
+//       return res.status(500).json({ err: err.message });
+//     });
+// });
+
+app.patch("/api/tickets/:ticketId/done", async (req, res) => {
+  let id = req.params.ticketId;
+  try {
+    Ticket.find({}).then((result) => {
+      for (let ticket of result) {
+        if (ticket._id == id) {
+          console.log(ticket.done);
+          let obj = ticket;
+          obj.done = true;
+          console.log(obj);
+          Ticket.update({ _id: ObjectId(id) }, { $set: updateObject });
+          res.json({ updated: true });
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "error ocurred: ", error: error.message });
+  }
+});
+
 module.exports = app;
